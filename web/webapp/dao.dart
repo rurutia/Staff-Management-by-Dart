1701de5dfea545d3e84daa5d6898f7602f7becab
rurutia <haoyumichael@gmail.com>
2013-04-02 23:05:31 +1100

    Data Access Object Interface

--- web/webapp/dao.dart
+++ web/webapp/dao.dart
@@ -4,12 +4,16 @@ part of staff_management;
 
 // Data Access Object interface
 abstract class Dao {
-
+   
+   // read number of count staffs from data store from start index	
    String getStaffs(int start, int count);
+   // search staffs by keyword from start index 
    String searchStaffs(int start, int count, String keyword);
+   // add new staff to data store
    String addNewStaff(int employeeNo, String name, String position, int yearJoin);
+   // delete staffs from data store
    String deleteStaffsByIDs(String ids);
-   // recover original staffs data from delete operation
+   // recover original staffs data from deletion
    void recoverStaffs();
    
 }
